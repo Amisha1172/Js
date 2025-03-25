4f03c32cfa842e57f7c98f63f84adf6038788043
Amisha1172 <amishasah223@gmail.com>
2025-03-25 16:23:54 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// console.log("amisha");
+// var a=5;
+// a=a+1;
+// console.log(a);
+// const b=5
+// // b=b+1
+// console.log(b);
+
+ let a=10
+let b=20
+ let c=a+b;
+console.log(c);
+a=20
+b=40
+c=a-b;
+console.log(c);
+console.log(10==10);
+console.log(10==5);
