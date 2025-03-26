dc4cbb9c17fc894f7c3b06876947d408d1b91306
Amisha1172 <amishasah223@gmail.com>
2025-03-26 13:31:03 +0545

    added operators

--- app.js
+++ app.js
@@ -12,7 +12,23 @@ let b=20
 console.log(c);
 a=20
 b=40
-c=a-b;
+// c=a-b;
+// c=a*b
+c=b/2
 console.log(c);
 console.log(10==10);
 console.log(10==5);
+console.log(4<=4)
+console.log(8<1)
+
+let x=20
+let y=4
+let z=x+y;
+console.log(z);
+z=x-y;
+console.log(z);
+z=x*y;
+console.log(z);
+z=x/y;
+console.log(z);
+console.log(10==10);
