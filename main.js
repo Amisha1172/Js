324df78b9c24dcc15c03f1213384cd8a0330d938
Amisha1172 <amishasah223@gmail.com>
2025-03-26 16:17:17 +0545

    switch case

---
+++ main.js
@@ -0,0 +1,54 @@
+// let a=1;
+
+// if(a==10){
+//     console.log("a");
+// }
+// else if(a==5){
+//     console.log("b");
+// }
+// else if(a==6){
+//     console.log("c");
+// }
+// else{
+//     console.log("d");
+// }
+
+// let a=13;
+// if(a%2==0){
+//     console.log("odd")
+// }
+
+// else{
+// console.log("odd");
+// }
+// let a = 98;
+// let b = 10;
+// if(a>b){
+//     console.log("a is greater");
+
+// }
+// else{
+//     console.log("b is greater");
+// }
+
+let a ="c";
+
+switch (a){
+    case "a":
+        console.log("apple");
+        break;
+    case "b":
+        console.log("ball");
+        break;
+    case "c":
+        console.log("cat");
+        break;
+    case "d":
+        console.log("dog");
+        break;
+    case "e":
+        console.log("elephant") 
+        break;  
+    default:
+        console.log("Not valid")
+}
