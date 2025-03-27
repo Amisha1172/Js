b7b38e43d0b069b9441ace185a43b20619626c6a
Amisha1172 <amishasah223@gmail.com>
2025-03-27 12:49:34 +0545

    added variables

--- main.js
+++ main.js
@@ -1,54 +1,93 @@
-// let a=1;
+// // let a=1;
 
-// if(a==10){
-//     console.log("a");
-// }
-// else if(a==5){
-//     console.log("b");
-// }
-// else if(a==6){
-//     console.log("c");
-// }
-// else{
-//     console.log("d");
+// // if(a==10){
+// //     console.log("a");
+// // }
+// // else if(a==5){
+// //     console.log("b");
+// // }
+// // else if(a==6){
+// //     console.log("c");
+// // }
+// // else{
+// //     console.log("d");
+// // }
+
+// // let a=13;
+// // if(a%2==0){
+// //     console.log("odd")
+// // }
+
+// // else{
+// // console.log("odd");
+// // }
+// // let a = 98;
+// // let b = 10;
+// // if(a>b){
+// //     console.log("a is greater");
+
+// // }
+// // else{
+// //     console.log("b is greater");
+// // }
+
+// let a ="c";
+
+// switch (a){
+//     case "a":
+//         console.log("apple");
+//         break;
+//     case "b":
+//         console.log("ball");
+//         break;
+//     case "c":
+//         console.log("cat");
+//         break;
+//     case "d":
+//         console.log("dog");
+//         break;
+//     case "e":
+//         console.log("elephant") 
+//         break;  
+//     default:
+//         console.log("Not valid")
 // }
 
-// let a=13;
-// if(a%2==0){
-//     console.log("odd")
-// }
-
-// else{
-// console.log("odd");
-// }
-// let a = 98;
-// let b = 10;
-// if(a>b){
-//     console.log("a is greater");
-
-// }
-// else{
-//     console.log("b is greater");
-// }
-
-let a ="c";
-
-switch (a){
-    case "a":
-        console.log("apple");
-        break;
-    case "b":
-        console.log("ball");
-        break;
-    case "c":
-        console.log("cat");
-        break;
-    case "d":
-        console.log("dog");
-        break;
-    case "e":
-        console.log("elephant") 
-        break;  
-    default:
-        console.log("Not valid")
+switch (1){
+    case "1":
+    console.log("Baishakh")
+    break;
+    case "2":
+    console.log("Jestha")
+    break;
+    case "3":
+    console.log("Ashar")
+    break;
+    case "4":
+    console.log("Shrawan")
+    break;
+    case "5":
+    console.log("Bhadra")
+    break;
+    case "6":
+    console.log("Ashoj")
+    break;
+    case "7":
+    console.log("Kartik")
+    break;
+    case "8":
+    console.log("Mansir")
+    break;
+    case "9":
+    console.log("Push")
+    break;
+    case "10":
+    console.log("Magh")
+    break;
+    case "11":
+    console.log("Falgun")
+    break;
+    case "12":
+    console.log("chaitra")
+    break;
 }
---
+++ new.js
@@ -0,0 +1,26 @@
+// var age = 25;
+
+// if(true){
+//     console.log(age);
+// }
+// var age = 25;
+// if(true){
+//     console.log(age);
+// }
+
+// function solve(){
+//     var age = 25;
+//     console.log(age);
+// }
+// console.log(age);
+// solve();
+
+// var x = 10;
+// var y = 20;
+// {
+// let a = 10;
+// console.log(a);
+// }
+
+const a = 28;
+console.log(a);
