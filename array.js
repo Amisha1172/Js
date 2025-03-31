7a72e1b60f9fd0cab90cf6a60be8fc750d9e1e9d
Amisha1172 <amishasah223@gmail.com>
2025-03-31 22:16:50 +0545

    added array

---
+++ array.js
@@ -0,0 +1,15 @@
+ let a = [1,2,3,4,5,6,7]
+ let b = ["a","b","c","d"]
+ console.log(a[4]);
+
+
+//  Using Array method 
+
+// pop using
+const number = [1,2,3,4,5,6,7]
+console.log(number.pop());
+console.log(3);
+
+// push usinsg
+const animal =["cow","dog","Buffalo"]
+
---
+++ loop2.js
@@ -0,0 +1,11 @@
+// let i= 6;
+
+// for (x=1; x<=10; x++){
+//     console.log("6 x",x,"=",x*6);
+// }
+
+for(let a=1; a<=10; a++){
+    console.log("9X",a,"=",9*a );
+}
+
+ 
