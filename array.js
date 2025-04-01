0ba45a0a89176679f9d9485118381b2c402f51ee
Amisha1172 <amishasah223@gmail.com>
2025-04-01 16:09:15 +0545

    added array method

--- array.js
+++ array.js
@@ -1,15 +1,94 @@
- let a = [1,2,3,4,5,6,7]
- let b = ["a","b","c","d"]
- console.log(a[4]);
+//  let a = [1,2,3,4,5,6,7]
+//  let b = ["a","b","c","d"]
+//  console.log(a[4]);
+//  console.log(a[2]);
+//  console.log(a[5 ]);
+
+//  using Array method
+
+// let a = ["Apple", "Banana", "Mango","Orange"]
+// console.log(a[0]);
+// console.log(a[3]);
+
+// Using map method
+
+// let a = [1,4,8,10];
+// let b = a.map((e)=>{
+// return e+2;
+
+// })
+
+// console.log(b);
+
+// USing filter
+
+// let a = [ 2,6,7,4,8,9,10];
+// let b = a.filter((c=>{
+//     return c%3==0
+// }));
+// console.log(b);
+
+// let a = [2,6, 7,4,8,10];
+// let b = a.filter((c)=>c%3==0)
+// console.log(b);
 
 
-//  Using Array method 
+// Using sort method
 
-// pop using
-const number = [1,2,3,4,5,6,7]
-console.log(number.pop());
-console.log(3);
+// let a = [6,8, 7,4,10,13];
+// a.sort();
+// console.log(a);
 
-// push usinsg
-const animal =["cow","dog","Buffalo"]
+// USing slice
+
+// let a = ["apple","banana", "mango", "egg"]
+// let b = a.slice(1);
+// let c = a.slice(0,2);
+// console.log(c);
+
+// Using find method
+// let a = [5,6,4,8,10];
+// let b = a.find((c)=> c>8);
+// console.log(b);
+
+// Using splice 
+
+// syntax (itemIndex,delete count,value);
+// let a = ["apple","mango","banana","orange"];
+// let b = a.splice(2,1,"pineapple");
+// console.log(a);
+// console.log(b);
+
+// using reduce 
+// let a = [1,4,6,8,7,6];
+// }
+
+// let b = a.reduce((accumulator,item)=>{
+// return accumulator+item;
+// },0)
+// console.log(b);
+
+
+// using object
+
+// let a = {
+//     name :"amisha",
+//     age :"20"
+// }
+// console.log(a.name);
+
+// using objects 
+let x = [7,8,9]
+let a = [
+    {
+        name:"amisha",
+        class:1,
+        present:true
+    },
+    {
+        name:"ram",
+        class:3,
+    }
+]
+console.log(x[1]);
 
