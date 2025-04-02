8e62729ff179e9485ce8798aabc535a0661e3f3c
Amisha1172 <amishasah223@gmail.com>
2025-04-02 12:22:59 +0545

    added contitional statements

--- operators.js
+++ operators.js
@@ -8,8 +8,8 @@
 //   let b = 5;
 //   console.log(a%b);
 
-let a = 2;
-let b = 5;
+// let a = 2;
+// let b = 5;
 // console.log(a**b);
 // a = a+10;
 // a= -10;
@@ -19,8 +19,55 @@ let b = 5;
 
 // Using comparision operators
 
-console.log(5>= 5);
+// console.log(5>= 5);
 
+// create conditional statements`
 
+//  let age = 30;
+//  if(age  >= 18){
+//     console.log("can vote"); 
+//  }
+//  else {
+// console.log("can not vote");
+//  }
 
-  
+// let number = 5;
+// let number = 14;
+// if(number ==1){
+//     console.log("A");
+    
+// }
+// else if(number == 2){
+//     console.log("B");
+// }
+// else if(number == 3){
+//     console.log("C");
+// }
+// else if(number == 4){
+//     if(age >18){
+//         console.log("I can vote");0
+//     }
+//     console.log("D");
+// }
+// else if(number == 5){
+//     console.log("E");
+// }
+// else {
+//     console.log("F");
+// }
+
+// create switch case
+
+let num = 3;
+switch(num) {
+    case 1: console.log("A");
+    break;
+    case 2: console.log("B");
+    break;
+    case 3: console.log("c");
+    // break;
+    case 4: console.log("D");
+    break;
+    default: console.log("F");
+    
+}
